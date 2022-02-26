fd53d10a0d7eeadc3a31fa18a4a4d8bc636eac04
TJessada <99568497+TJessada@users.noreply.github.com>
2022-02-26 22:42:42 +0700

    Delete flutterapi directory

--- flutterapi/flutterapi/myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Todolist(models.Model):
-    title = models.CharField(max_length=100) # ไม่ได้ใส่เหมือนบรรทัดล่าง แสดงว่าบังคับใส่ field นี้
-    detail = models.TextField(null=True, blank=True) # ใส่ null=true , blank=true แสดงว่าไม่บังคับใส่
-
-    def __str__(self): #เพิ่มหัวข้อในหน้า admin จะได้อ่านง่าย
-        return self.title #ถ้าเปลี่ยนข้อมูล file นี้ save แล้ว ให้ makemigrations และ migrate อีกครั้ง
-
